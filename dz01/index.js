1daca9f3382880537b64b56ed5c789530be4d7ed
AleksandraGunbina1~ <aleksandragunbina@mail.ru>
2024-03-02 10:35:16 +0300

    chore: round of solutions, add \n

--- dz01/index.js
+++ dz01/index.js
@@ -22,19 +22,19 @@ const onEquationCoeffView = (coeff) => {
 };
 
 if (coefficients.length < 1) {
-  stdout.write("Коэффициенты не введены");
+  stdout.write("Коэффициенты не введены\n");
   process.exit(1);
 } else if (coefficients.length > 3) {
-  stdout.write("Введено слишком много коэффициентов");
+  stdout.write("Введено слишком много коэффициентов\n");
   process.exit(40);
 } 
 else if (a === 0) {
   stderr.write(
-    "Это не квадратное уравнение, старший коэффициент не может быть равен нулю"
+    "Это не квадратное уравнение, старший коэффициент не может быть равен нулю\n"
   );
   process.exit(41);
 } else if (coefficients.every(item => !isNaN(item)) == false) {
-  stderr.write("Один из введенных коэффициент не является числом");
+  stderr.write("Один из введенных коэффициент не является числом\n");
   process.exit(42);
 } else {
   console.log(
@@ -45,25 +45,25 @@ else if (a === 0) {
   const discriminant = b ** 2 - 4 * a * c;
   switch (true) {
     case discriminant > 0: {
-      const solutionFirst = (-b - Math.sqrt(discriminant)) / (2 * a);
-      const solutionSecond = (-b + Math.sqrt(discriminant)) / (2 * a);
+      const solutionFirst = ((-b - Math.sqrt(discriminant)) / (2 * a)).toFixed(3);
+      const solutionSecond = ((-b + Math.sqrt(discriminant)) / (2 * a)).toFixed(3);
       stdout.write(
-        `Уравнение имеет два решения: ${solutionFirst} и ${solutionSecond}`
+        `Уравнение имеет два решения: ${solutionFirst} и ${solutionSecond}\n`
       );
       process.exit(0);
     }
     case discriminant === 0: {
-      const solution = -b / (2 * a);
-      stdout.write(`Уравение иммет одно (два одинаковых) решение: ${solution}`);
+      const solution = (-b / (2 * a)).toFixed(3);
+      stdout.write(`Уравение иммет одно (два одинаковых) решение: ${solution}\n`);
       process.exit(0);
     }
     case discriminant === 0: {
-      const solution = -b / (2 * a);
-      stdout.write(`Уравение иммет одно (два одинаковых) решение: ${solution}`);
+      const solution = (-b / (2 * a)).toFixed(3);
+      stdout.write(`Уравение иммет одно (два одинаковых) решение: ${solution}\n`);
       process.exit(0);
     }
     case discriminant < 0: {
-      stderr.write("Уравнение не имеет решений");
+      stderr.write("Уравнение не имеет решений\n");
       process.exit(50);
     }
   }
